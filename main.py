67097812f4c1d851d6f0588dd5f271628b7bab94
ArchAshly <130307958+ArchAshly@users.noreply.github.com>
2024-01-07 12:27:03 +0200

    Fixed database insertion

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from website import create_app
+from website.models import Food, db
 
 app = create_app()
 
-if __name__ == '__main__':
+with app.app_context():
+    if db.session.query(Food).count() == 0:
+        Food.import_food_data("website/Food_Alergy.csv")
+
+if __name__ == "__main__":
     app.run(debug=True)
